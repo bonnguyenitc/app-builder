907f7496f9c51012a7f9380172a5a9b49f3f8e55
Thoai Nguyen <ntthoaiit@gmail.com>
2026-01-23 14:58:14 +0700

    Update event close app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -24,6 +24,12 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .on_window_event(|window, event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+                window.hide().unwrap();
+                api.prevent_close();
+            }
+        })
         .setup(|app| {
             let conn = init_db(app.handle()).expect("Failed to initialize database");
             app.manage(DbState(Mutex::new(conn)));
@@ -51,6 +57,14 @@ pub fn run() {
             update_credential,
             delete_credential_by_id
         ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|app_handle, event| {
+            if let tauri::RunEvent::Reopen { .. } = event {
+                if let Some(window) = app_handle.get_webview_window("main") {
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+            }
+        });
 }
